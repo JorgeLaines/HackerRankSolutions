ddd5a3d50689d31ff2d4fee779c4f6a1e70d59a7
JorgeLaines <jorgelaines@gmail.com>
2023-04-17 19:33:13 -0500

    MigratoryBird

---
+++ HackerRank.Algorithms/MigratoryBird.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.Algorithms
+{
+    public static class MigratoryBird
+    {
+        public static int MigratoryBirds(List<int> arr)
+        {
+            // Some arrays do not work properly if the array is not previously sorted
+            // If this line becomes too heavy to process it can be removed but the process sections needs to be reviewed in order for this to work
+            arr.Sort();
+
+            // This is a good way to aggregate or count items in an array
+            Dictionary<int, int> aggregatedArray = arr
+            .GroupBy(item => item)
+            .ToDictionary(item => item.Key, item => item.Count());
+
+            // At this point we need to iterate through the dictionary to get the highest value, this process can be used to find the lowest too
+            KeyValuePair<int, int> highestCountTuple = aggregatedArray.First();
+            foreach (KeyValuePair<int, int> tuple in aggregatedArray)
+            {
+                if (tuple.Value > highestCountTuple.Value) highestCountTuple = tuple;
+            }
+
+            return highestCountTuple.Key;
+        }
+    }
+}
